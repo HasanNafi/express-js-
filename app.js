b0fcc5dc7e804e6645006e7de1e53cbf7533e37b
Hasan Nafi <work.nafi@gmail.com>
2021-10-24 10:33:34 +0600

    update app.js

--- app.js
+++ app.js
@@ -4,7 +4,19 @@ const app = express();
 const port = 8000;
 
 app.get("/", function(req, res) {
-    res.send("New express js app");
+    res.send("Home page");
+})
+
+app.get("/about", function(req, res) {
+    res.send("About page");
+})
+
+app.get("/contact", function(req, res) {
+    res.send("Contact page");
+})
+
+app.get("/service", function(req, res) {
+    res.send("Service page");
 })
 
 app.listen(port, function() {
