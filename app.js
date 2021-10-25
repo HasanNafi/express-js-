75aa2d1d4015a1d4bf46fe6a525872aee95caa27
Hasan Nafi <work.nafi@gmail.com>
2021-10-25 06:25:44 +0600

    added response status code

--- app.js
+++ app.js
@@ -3,8 +3,13 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+
+//res.send()  indicate to the body 
+//res.end()  indicate to the end situation of the present code 
+
 app.get("/", function(req, res) {
     res.send("Home page");
+    //res.status(401).send("simple string")    for changing  the status code
 })
 
 app.post("/about", function(req, res) {
