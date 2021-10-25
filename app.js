ff4e0785b5053970e94c8f31a47736df771a5b9a
Hasan Nafi <work.nafi@gmail.com>
2021-10-25 07:04:44 +0600

    Response redirect

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const port = 8000;
 //res.send()  indicate to the body 
 //res.end()  indicate to the end situation of the present code 
 
-
+//......................................................................//
+//......................................................................//
 
 //Response status code
 app.get("/", function(req, res) {
@@ -15,6 +16,9 @@ app.get("/", function(req, res) {
     //res.status(401).send("simple string")    for changing  the status code
 })
 
+//......................................................................//
+//......................................................................//
+//different methodes
 app.post("/about", function(req, res) {
     res.send("About page");
 })
@@ -28,41 +32,50 @@ app.delete("/service", function(req, res) {
 })
 
 
-
+//......................................................................//
+//......................................................................//
 //Response JSON
 
 app.get("/JSON", function(req, res) {
-    //res.send("JSON response code");
-    let myJson = [{
-            name: "Anik",
-            occupaion: "Engr.",
-            age: "24"
-        },
-        {
-            name: "Hasan",
-            occupaion: "Engr.",
-            age: "24"
-        },
-        {
-            name: "Nafi",
-            occupaion: "Engr.",
-            age: "24"
-        }
-    ]
-    res.json(myJson);
-})
-
-
-//Response Download
+        //res.send("JSON response code");
+        let myJson = [{
+                name: "Anik",
+                occupaion: "Engr.",
+                age: "24"
+            },
+            {
+                name: "Hasan",
+                occupaion: "Engr.",
+                age: "24"
+            },
+            {
+                name: "Nafi",
+                occupaion: "Engr.",
+                age: "24"
+            }
+        ]
+        res.json(myJson);
+    })
+    //......................................................................//
+    //......................................................................//
+    //Response Download
 
 app.get("/download", function(req, res) {
-    res.download("puppy.jpg");
+        res.download("puppy.jpg");
+    })
+    //......................................................................//
+    //......................................................................//
+    //Response Redirect
+
+app.get("/Bangladesh", function(req, res) {
+    res.redirect("http://localhost:8000/India");
 })
 
-
-
-
-
+app.get("/India", function(req, res) {
+        res.send("this is India");
+    })
+    //......................................................................//
+    //......................................................................//
 
 
 app.listen(port, function() {
